948917fab758b62149c7abe4bf23c670333b006f
christos154 <61019792+christos154@users.noreply.github.com>
2020-03-12 00:30:01 +0200

    crouch

--- Family Heirloom/Assets/TEst/player_MOVEMENT_christos.cs
+++ Family Heirloom/Assets/TEst/player_MOVEMENT_christos.cs
@@ -16,7 +16,12 @@ public class player_MOVEMENT_christos : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isgrounded;
-    
+
+    public Transform t_mesh;
+    public bool IsCrouch = false;                
+    public float LocalScaleY;                   
+    public float ControllerHeight;
+
 
     // Update is called once per frame
     void Update()
@@ -36,6 +41,18 @@ public class player_MOVEMENT_christos : MonoBehaviour
         {
             speed = walkspeed;
         }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            t_mesh.localScale = new Vector3(1, LocalScaleY, 1);
+            Controller.height = ControllerHeight;
+            speed = crouchspeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            t_mesh.localScale = new Vector3(1, 1, 1);
+            Controller.height = 2.8f;
+            speed = walkspeed;
+        }
 
         Vector3 move = transform.right * x + transform.forward * z;
         Controller.Move(move * speed * Time.deltaTime);
@@ -48,4 +65,5 @@ public class player_MOVEMENT_christos : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         Controller.Move(velocity * Time.deltaTime);
     }
+    
 }
