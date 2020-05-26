de034d9b3f78c51256f1971d6857973461db1177
Spudward <50043883+Spudward@users.noreply.github.com>
2020-05-26 01:57:46 +0100

    1

---
+++ Family Heirloom/Assets/Scripts/HitBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBehaviour : MonoBehaviour
+{
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<Unit>().myCondition = Unit.Conditions.STUNNED;
+        }
+    }
+}
